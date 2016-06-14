58d181487591356672bf77acb1a47e9872f4987c
Aline Gomes <Aline Gomes@DESKTOP-FDQ7M12>
2016-06-13 23:12:27 -0300

    Teste Aline2

--- src/br/upe/poo/clinica/entidades/Exames.java
+++ src/br/upe/poo/clinica/entidades/Exames.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 import java.util.Objects;
 
 public class Exames implements Serializable {
-    //Aline
+    //Aline 
 	private String dataExame;
 	private String horaExame;
 	private String tipoExame;
